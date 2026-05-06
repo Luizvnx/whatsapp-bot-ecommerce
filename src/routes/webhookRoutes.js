fc17d76f052efc13545c3aa4e0d2e039836320aa
Luiz <luizvnx@gmail.com>
2026-05-06 00:50:37 -0300

    bug fix

--- src/routes/webhookRoutes.js
+++ src/routes/webhookRoutes.js
@@ -4,4 +4,10 @@ const WebhookController = require('../controllers/WebhookController');
 
 router.post(['/evolution', '/evolution/messages-upsert'], WebhookController.handleEvolutionWebhook);
 
+router.post('/evolution/connection-update', (req, res) => {
+    const status = req.body?.data?.state || 'desconhecido';
+    console.log(`📡 Status da conexão do WhatsApp: ${status}`);
+        res.status(200).send('OK');
+});
+
 module.exports = router;
